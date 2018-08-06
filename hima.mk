c29dd4001f4268313b23c371f2121b93d326b6c7
Ethan Chen <intervigil@gmail.com>
2018-08-05 22:14:04 -0700

    hima-common: Convert power HAL for hima
    Change-Id: I5fe7a9fd0a5edf65fafe1e98b64e25d155be14cf

--- hima.mk
+++ hima.mk
@@ -253,7 +253,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    power.qcom
 
 # QMI
 PRODUCT_PACKAGES += \
