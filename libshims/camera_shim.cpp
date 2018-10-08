chaosmaster/android_device_htc_hima-common

hima-common: Add camera shim for P
Change-Id: I21f56be5aa437820c60a7ebce953218515c5f1c4

libshims/camera_shim.cpp
@@ -14,59 +14,115 @@
  * limitations under the License.
  */
 
+#include <gui/BufferQueue.h>
+#include <gui/SurfaceComposerClient.h>
+#include <utils/Errors.h>
+#include <utils/String8.h>
+#include <utils/StrongPointer.h>
+
 #include <string>
 
-//using namespace android;
+using android::sp;
+using android::status_t;
+using android::IBinder;
+using android::IGraphicBufferConsumer;
+using android::IGraphicBufferProducer;
+using android::PixelFormat;
+using android::SurfaceControl;
+using android::SurfaceComposerClient;
+
+android::SurfaceComposerClient::Transaction *t;
+
+// BufferItemConsumer(const sp<IGraphicBufferConsumer& consumer,
+//                    uint64_t consumerUsage,
+//                    int bufferCount = DEFAULT_MAX_BUFFERS,
+//                    bool controlledByApp = false)
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(
+    const sp<IGraphicBufferConsumer>& consumer, uint64_t consumerUsage,
+    int bufferCount, bool controlledByApp);
+
+extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
+    const sp<IGraphicBufferConsumer>& consumer, uint32_t consumerUsage,
+    int bufferCount, bool controlledByApp) {
+  _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(
+      consumer, consumerUsage, bufferCount, controlledByApp);
+}
+
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
+    sp<IGraphicBufferProducer>* outProducer,
+    sp<IGraphicBufferConsumer>* outConsumer,
+    void*) {
+  // createBufferQueue is a static method, call it directly
+  android::BufferQueue::createBufferQueue(outProducer, outConsumer);
+}
 
 // GraphicBuffer(uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
 //               uint32_t inUsage, std::string requestorName = "<Unknown>");
 extern "C" void _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
-    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage,
+    uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat, uint32_t inUsage,
     std::string requestorName);
 
 extern "C" void _ZN7android13GraphicBufferC1Ejjij(
-    uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage) {
+    uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
+    uint32_t inUsage) {
   std::string requestorName = "<Unknown>";
   _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
       inWidth, inHeight, inFormat, inUsage, requestorName);
 }
 
-extern "C" void _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEENS1_INS_22IGraphicBufferProducerEEE(
-    uint32_t token, uint32_t bufferProducer);
-//    const sp<IBinder>& token, sp<IGraphicBufferProducer> bufferProducer);
-
 extern "C" void _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEERKNS1_INS_22IGraphicBufferProducerEEE(
-    uint32_t token, uint32_t bufferProducer) {
-//    const sp<IBinder>& token, const sp<IGraphicBufferProducer>& bufferProducer) {
-  _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEENS1_INS_22IGraphicBufferProducerEEE(
-      token, bufferProducer);
+    const sp<IBinder>& token, const sp<IGraphicBufferProducer>& bufferProducer) {
+  // setDisplaySurface is a static method, call it directly
+  t->setDisplaySurface(token, bufferProducer);
+  //return t.apply();
 }
 
-extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
-    void *outProducer, void *outConsumer, bool consumerIsSurfaceFlinger);
+// sp<SurfaceControl> createSurface(const String8& name, uint32_t w, uint32_t h,
+//                                  PixelFormat format, uint32_t flags = 0,
+//                                  SurfaceControl* parent = nullptr,
+//                                  uint32_t windowType = 0,
+//                                  uint32_t ownerUid = 0);
+extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
+    const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
+    uint32_t flags, SurfaceControl* parent, uint32_t windowType,
+    uint32_t ownerUid);
 
-extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
-    void *outProducer, void *outConsumer, void *allocator __unused) {
-  _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
-      outProducer, outConsumer, false);
+extern sp<android::SurfaceControl> ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(
+    const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
+    uint32_t flags) {
+  sp<SurfaceControl> s;
+  s->getClient()->createSurfaceChecked(
+      name, w, h, format, &s, flags, nullptr, 0, 0);
+  return s;
 }
 
-extern "C" void _ZN7android18BufferItemConsumerC1ENS_2spINS_22IGraphicBufferConsumerEEEjib(
-    uint32_t consumer, uint32_t consumerUsage, int bufferCount, bool controlledByApp) {
-  _ZN7android18BufferItemConsumerC1ENS_2spINS_22IGraphicBufferConsumerEEEjib(
-      consumer, consumerUsage, bufferCount, false);
+// status_t setLayer(int32_t layer);
+/* needed for N-libs
+extern "C" status_t _ZN7android14SurfaceControl8setLayerEi(int32_t layer);
+
+extern "C" status_t _ZN7android14SurfaceControl8setLayerEj(uint32_t layer) {
+  return _ZN7android14SurfaceControl8setLayerEi(layer);
+}
+*/
+
+extern "C" void _ZN7android5FenceD1Ev() { }
+
+extern "C" void _ZN7android21SurfaceComposerClient21openGlobalTransactionEv() {
+  t = new(android::SurfaceComposerClient::Transaction);
+}
+extern "C" void _ZN7android21SurfaceComposerClient20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES8_(
+    const sp<IBinder>& token,
+    uint32_t orientation,
+    const android::Rect& layerStackRect,
+    const android::Rect& displayRect) { 
+  t->setDisplayProjection(token, orientation, layerStackRect, displayRect);
 }
 
-extern "C" void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib
-(
-     //   const sp<IGraphicBufferConsumer>&consumer, unsigned int, int, bool)
-     uint64_t consumer, uint64_t consumerUsage, int bufferCount, bool controlledByApp){
-     _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(
-     consumer, consumerUsage, bufferCount, false);
+extern "C" void _ZN7android21SurfaceComposerClient20setDisplayLayerStackERKNS_2spINS_7IBinderEEEj(
+    const sp<IBinder>& token, uint32_t layerStack){
+  t->setDisplayLayerStack(token, layerStack);
 }
 
-// android::Fence::~Fence()
-extern "C" void _ZN7android5FenceD1Ev() {
-  // no-op, the explicit destructor was replaced with = default;
+extern "C" void _ZN7android21SurfaceComposerClient22closeGlobalTransactionEb(){
+  t->apply();
 }
-

AICP/device_htc_hima-common

libshims/camera_shim.cpp
@@ -29,6 +29,9 @@ using android::IGraphicBufferConsumer;
 using android::IGraphicBufferProducer;
 using android::PixelFormat;
 using android::SurfaceControl;
+using android::SurfaceComposerClient;
+
+android::SurfaceComposerClient::Transaction *t;
 
 // BufferItemConsumer(const sp<IGraphicBufferConsumer& consumer,
 //                    uint64_t consumerUsage,
@@ -67,10 +70,10 @@ extern "C" void _ZN7android13GraphicBufferC1Ejjij(
       inWidth, inHeight, inFormat, inUsage, requestorName);
 }
 
-extern "C" status_t _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEERKNS1_INS_22IGraphicBufferProducerEEE(
+extern "C" void _ZN7android21SurfaceComposerClient17setDisplaySurfaceERKNS_2spINS_7IBinderEEERKNS1_INS_22IGraphicBufferProducerEEE(
     const sp<IBinder>& token, const sp<IGraphicBufferProducer>& bufferProducer) {
   // setDisplaySurface is a static method, call it directly
-  return android::SurfaceComposerClient::setDisplaySurface(token, bufferProducer);
+  t->setDisplaySurface(token, bufferProducer);
 }
 
 // sp<SurfaceControl> createSurface(const String8& name, uint32_t w, uint32_t h,
@@ -83,11 +86,13 @@ extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8
     uint32_t flags, SurfaceControl* parent, uint32_t windowType,
     uint32_t ownerUid);
 
-extern "C" void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(
+extern sp<android::SurfaceControl> ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(
     const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
     uint32_t flags) {
-  return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
-      name, w, h, format, flags, nullptr, 0, 0);
+  sp<SurfaceControl> s;
+  s->getClient()->createSurfaceChecked(
+      name, w, h, format, &s, flags, nullptr, 0, 0);
+  return s;
 }
 
 // status_t setLayer(int32_t layer);
@@ -102,4 +107,24 @@ extern "C" status_t _ZN7android14SurfaceControl8setLayerEj(uint32_t layer) {
 // android::Fence::~Fence()
 extern "C" void _ZN7android5FenceD1Ev() {
   // no-op, the explicit destructor was replaced with = default;
+}
+
+extern "C" void _ZN7android21SurfaceComposerClient21openGlobalTransactionEv() {
+  t = new(android::SurfaceComposerClient::Transaction);
+}
+extern "C" void _ZN7android21SurfaceComposerClient20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES8_(
+    const sp<IBinder>& token,
+    uint32_t orientation,
+    const android::Rect& layerStackRect,
+    const android::Rect& displayRect) {
+  t->setDisplayProjection(token, orientation, layerStackRect, displayRect);
+}
+
+extern "C" void _ZN7android21SurfaceComposerClient20setDisplayLayerStackERKNS_2spINS_7IBinderEEEj(
+    const sp<IBinder>& token, uint32_t layerStack){
+  t->setDisplayLayerStack(token, layerStack);
+}
+
+extern "C" void _ZN7android21SurfaceComposerClient22closeGlobalTransactionEb(){
+  t->apply();
 }