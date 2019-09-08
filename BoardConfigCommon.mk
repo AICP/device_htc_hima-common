c82b22112ce4f4c091b5c72302bafcdf7315fe76
Julian Veit <claymore1298@gmail.com>
2019-09-08 23:19:57 +0200

    DNM: hima-common: sepolicy: haxx ... again
    Change-Id: I193b205134918de8c26c284d31e9d06836c56291

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -166,7 +166,13 @@ BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 
 # SELinux
 #include device/qcom/sepolicy-legacy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
